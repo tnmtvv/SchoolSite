e173a303dd588bae4c4147aaaae84525b59f7763
TanyaMatveeva <62231627+tnmtvv@users.noreply.github.com>
2023-11-02 02:37:33 +0300

    intensive done

--- schoolsite/src/components/Intensive/Intensive.js
+++ schoolsite/src/components/Intensive/Intensive.js
@@ -1,38 +1,40 @@
 import React from 'react';
 import { Image, Button, Group, Text } from '@mantine/core';
 import './Intensive.css'
-import img from '../../images/english-speaking-club.jpg'
+import img from '../../images/camp.jpg'
 
 
 const Intensive = () => {
     return (
         <div className="main-block">
-            <div className="text-image">
-                <div className="text-block">
-                    <div className="title">
-                        Интенсивы английского языка для детей и подростков во время каникул
+            <div className='all-content'>
+                <div className="text-image">
+                    <div className="text-block">
+                        <div className="title">
+                            Интенсивы английского языка для детей и подростков во время каникул
+                        </div>
+                        <div className="main-text">
+                            <Text>
+                                <b>Интенсив</b> – это отличная возможность для тех,
+                                кто хочет прокачать английский на каникулах,
+                                при этом получить удовольствие от занятий и не устать от бесконечной грамматики.
+                            </Text>
+                            <Text>
+                                <b>Основная задача курса</b> – заинтересовать ребят,
+                                вне зависимости от возраста и уровня английского,
+                                создать англоговорящую среду – важное условие для погружения в язык.
+                            </Text>
+                        </div>
                     </div>
-                    <div className="main-text">
-                        <Text>
-                            <b>Интенсив</b> – это отличная возможность для тех,
-                            кто хочет прокачать английский на каникулах,
-                            при этом получить удовольствие от занятий и не устать от бесконечной грамматики.
-                        </Text>
-                        <Text>
-                            <b>Основная задача курса</b> – заинтересовать ребят,
-                            вне зависимости от возраста и уровня английского,
-                            создать англоговорящую среду – важное условие для погружения в язык.
-                        </Text>
-                    </div>
-                </div>
-                <div>
-                    <Image src={img} alt='...' fill sizes="100vw, 100px" priority />
-                </div>
-            </div>
-            <Group className='block-simple-button' mt="md">
-                <Button className='simple-button' variant="filled">Подробнее</Button>
-            </Group>
 
+                    <div className='img'>
+                        <Image src={img} alt='...' priority />
+                    </div>
+                </div>
+                <Group className='block-simple-button' mt="md">
+                    <Button className='simple-button' variant="filled">Подробнее</Button>
+                </Group>
+            </div>
         </div>
     )
 }
--- schoolsite/src/pages/Children.js
+++ schoolsite/src/pages/Children.js
@@ -1,16 +1,28 @@
 import React from "react";
 import Cover from "../components/Cover/Cover";
-import { Title } from "@mantine/core";
+import { Table, Title } from "@mantine/core";
 import img from "../images/EnglishForChildrenCover.jpg"
 import Intensive from "../components/Intensive/Intensive";
+import PriceTable from "../components/PriceTable/PriceTable"
+import CourseMenu from "../components/CourseMenu/CourseMenu";
 
 const Children = () => {
     const title = <Title color="white">Английский<br />для детей</Title>
     return (
         <div>
-            <Cover title={title} img={img} />
-            <Intensive></Intensive>
+            <div>
+                <Cover title={title} img={img} />
+            </div>
+            <div>
+                <Intensive></Intensive>
+            </div>
+            {/* <div>
+                <CourseMenu></CourseMenu>
+            </div> */}
+
+            {/* <PriceTable></PriceTable> */}
         </div>
+
     )
 }
 
