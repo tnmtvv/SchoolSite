c13bf0214165a4454d3f3c5f91855a134d4917d9
TanyaMatveeva <62231627+tnmtvv@users.noreply.github.com>
2023-12-02 01:04:39 +0300

    grid done

---
+++ schoolsite/src/components/SimpleImgLink/SimpleImgLink.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import { Image, Button, Group, BackgroundImage, Text } from '@mantine/core';
+import "./SimpleImgLink.css"
+
+
+const SimpleImgLink = (props) => {
+    var option_text = props.text;
+    var img = props.img
+
+    return (
+
+        <div className="simple-img-link">
+            <Link to='/Adults'>
+                <div className='background-image'>
+                    {/* <img className="menu-image" scr={img} fit="fill" /> */}
+                    <img className="menu-image" src={img}></img>
+                    <div className='text-over-image'>
+                        {option_text}
+                    </div>
+                </div>
+
+            </Link>
+
+        </div>
+
+    );
+}
+
+export default SimpleImgLink;
--- schoolsite/src/pages/Home.js
+++ schoolsite/src/pages/Home.js
@@ -2,6 +2,8 @@ import React from "react";
 import { useState } from "react";
 import { Image } from '@mantine/core';
 import PriceTable from "../components/PriceTable/PriceTable"
+import CourseGrid from "../components/CoursesGrid/CoursesGrid";
+import SimpleImgLink from "../components/SimpleImgLink/SimpleImgLink";
 
 const Home = () => {
     const prices = [
@@ -13,14 +15,18 @@ const Home = () => {
     ];
 
     return (
-        <div style={{ display: "flex", justifyContent: "center" }}>
+        <div>
+            <CourseGrid />
+            {/* <SimpleImgLink /> */}
+            <div style={{ display: "flex", justifyContent: "center" }}>
 
-            {/* <Image
+                {/* <Image
                 radius="md"
                 src="https://images.unsplash.com/photo-1688920556232-321bd176d0b4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2370&q=80"
             /> */}
-            <PriceTable data={prices}></PriceTable>
+                <PriceTable data={prices}></PriceTable>
 
+            </div>
         </div>
     )
 }
