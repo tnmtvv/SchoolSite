d9d945d1a151f3b6356bfcb762f18ee558728185
TanyaMatveeva <62231627+tnmtvv@users.noreply.github.com>
2023-12-02 02:02:28 +0300

    Update Home.js

--- schoolsite/src/pages/Home.js
+++ schoolsite/src/pages/Home.js
@@ -17,15 +17,8 @@ const Home = () => {
     return (
         <div>
             <CourseGrid />
-            {/* <SimpleImgLink /> */}
             <div style={{ display: "flex", justifyContent: "center" }}>
-
-                {/* <Image
-                radius="md"
-                src="https://images.unsplash.com/photo-1688920556232-321bd176d0b4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2370&q=80"
-            /> */}
                 <PriceTable data={prices}></PriceTable>
-
             </div>
         </div>
     )
